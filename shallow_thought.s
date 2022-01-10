0398e521ae06302d65a717cc68f093cabffd0bbf
Willem van der Jagt <wkjagt@gmail.com>
2022-01-09 19:51:02 -0500

    add strings.s

--- shallow_thought.s
+++ shallow_thought.s
@@ -5,14 +5,10 @@
                 .import wait_for_key_press
                 .import send_byte_to_screen
                 .import xmodem_receive
+                .import print_formatted_byte_as_hex
+                .import print_string
 
 .import string_table
-
-.zeropage
-                ; todo: use .res 2 without it being output in the binary file
-tmp1            = $02
-tmp2            = $04
-tmp3            = $06
 
 xmodem_byte_sink_vector = $08
 
@@ -44,63 +40,64 @@ reset:
                 ;    transmission
                 jsr     wait_for_key_press
                 jsr     xmodem_receive
-
-; this only adds a space
-print_formatted_byte_as_hex:
-                jsr     print_byte_as_hex
-                lda     #' '
-                jsr     send_byte_to_screen
                 rts
 
-print_byte_as_hex:
-                pha                     ; keep a copy for the low nibble
+; ; this only adds a space
+; print_formatted_byte_as_hex:
+;                 jsr     print_byte_as_hex
+;                 lda     #' '
+;                 jsr     send_byte_to_screen
+;                 rts
 
-                lsr                     ; shift high nibble into low nibble
-                lsr
-                lsr
-                lsr
+; print_byte_as_hex:
+;                 pha                     ; keep a copy for the low nibble
 
-                jsr     print_nibble
+;                 lsr                     ; shift high nibble into low nibble
+;                 lsr
+;                 lsr
+;                 lsr
 
-                pla                     ; get original value back
-                and     #%00001111      ; reset high nibble
-                jsr     print_nibble
-                rts
+;                 jsr     print_nibble
 
-print_nibble:
-                cmp     #10
-                bcs     @letter         ; >= 10 (hex letter A-F)
-                adc     #48             ; ASCII offset to numbers 0-9
-                jmp     @print
-@letter:
-                adc     #54             ; ASCII offset to letters A-F
-@print:
-                jsr     send_byte_to_screen
-                rts
+;                 pla                     ; get original value back
+;                 and     #%00001111      ; reset high nibble
+;                 jsr     print_nibble
+;                 rts
 
-print_string:
-                asl                     ; multiply by 2 because size of memory address is 2 bytes
-                tay
-                lda     string_table,y  ; string index into string table
-                sta     tmp3            ; LSB
-                iny
-                lda     string_table,y
-                sta     tmp3+1          ; MSB
+; print_nibble:
+;                 cmp     #10
+;                 bcs     @letter         ; >= 10 (hex letter A-F)
+;                 adc     #48             ; ASCII offset to numbers 0-9
+;                 jmp     @print
+; @letter:
+;                 adc     #54             ; ASCII offset to letters A-F
+; @print:
+;                 jsr     send_byte_to_screen
+;                 rts
 
-                ldy #0
-@next_char:
-                lda (tmp3),y
-                beq @done
+; print_string:
+;                 asl                     ; multiply by 2 because size of memory address is 2 bytes
+;                 tay
+;                 lda     string_table,y  ; string index into string table
+;                 sta     tmp3            ; LSB
+;                 iny
+;                 lda     string_table,y
+;                 sta     tmp3+1          ; MSB
 
-                jsr send_byte_to_screen
-                iny
-                bra @next_char
-@done:
-                lda     #$0d
-                jsr     send_byte_to_screen
-                lda     #$0a
-                jsr     send_byte_to_screen
-                rts
+;                 ldy #0
+; @next_char:
+;                 lda (tmp3),y
+;                 beq @done
+
+;                 jsr send_byte_to_screen
+;                 iny
+;                 bra @next_char
+; @done:
+;                 lda     #$0d
+;                 jsr     send_byte_to_screen
+;                 lda     #$0a
+;                 jsr     send_byte_to_screen
+;                 rts
 
 ; strings ========================================
 
--- strings.s
+++ strings.s
@@ -1,4 +1,11 @@
-.export string_table
+.export print_formatted_byte_as_hex
+.export print_string
+
+.import send_byte_to_screen
+
+tmp1 = $02
+tmp2 = $04
+tmp3 = $06
 
 .code
 
@@ -7,3 +14,60 @@ string_table:
 
 s_startup:      .byte "Shallow Thought v0.01", 0                
 s_any_key:      .byte "Press any key", 0
+
+; this only adds a space
+print_formatted_byte_as_hex:
+                jsr     print_byte_as_hex
+                lda     #' '
+                jsr     send_byte_to_screen
+                rts
+
+print_byte_as_hex:
+                pha                     ; keep a copy for the low nibble
+
+                lsr                     ; shift high nibble into low nibble
+                lsr
+                lsr
+                lsr
+
+                jsr     print_nibble
+
+                pla                     ; get original value back
+                and     #%00001111      ; reset high nibble
+                jsr     print_nibble
+                rts
+
+print_nibble:
+                cmp     #10
+                bcs     @letter         ; >= 10 (hex letter A-F)
+                adc     #48             ; ASCII offset to numbers 0-9
+                jmp     @print
+@letter:
+                adc     #54             ; ASCII offset to letters A-F
+@print:
+                jsr     send_byte_to_screen
+                rts
+
+print_string:
+                asl                     ; multiply by 2 because size of memory address is 2 bytes
+                tay
+                lda     string_table,y  ; string index into string table
+                sta     tmp3            ; LSB
+                iny
+                lda     string_table,y
+                sta     tmp3+1          ; MSB
+
+                ldy #0
+@next_char:
+                lda (tmp3),y
+                beq @done
+
+                jsr send_byte_to_screen
+                iny
+                bra @next_char
+@done:
+                lda     #$0d
+                jsr     send_byte_to_screen
+                lda     #$0a
+                jsr     send_byte_to_screen
+                rts
