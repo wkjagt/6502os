3cef1b47cd7f253846853ab57dab6c59aa83724b
Willem van der Jagt <wkjagt@gmail.com>
2025-06-08 12:05:21 -0400

    all the things

--- tools/disassembler.s
+++ tools/disassembler.s
@@ -9,6 +9,69 @@ code_pointer:   .res 1
 
 .code
 
+dasm:           
+                ; lda     TERM_ARG1
+                ; bne     @arg_given
+                ; lda     #>__PROGRAM_START__
+                ; sta     TERM_ARG1
+
+
+; @arg_given:     jmp     (TERM_ARG_ADDR1)       ; get page from cli arg
+                ; lda     #0
+                sta     code_pointer
+                ; lda     #06
+                sta     code_pointer+1
+                ldx     #20
+.loop:          jsr     print_line
+                dex
+                bne     .loop
+                rts 
+
+;==============================================================
+; print one line of disassembled code, starting at the address
+; stored at code_pointer
+;==============================================================
+print_line:     phx
+                lda     code_pointer+1
+                jsr     JMP_PRINT_HEX
+                lda     code_pointer
+                jsr     JMP_PRINT_HEX
+                lda     #":"
+                jsr     JMP_PUTC
+                lda     #" "
+                jsr     JMP_PUTC
+                jsr     find_instruction
+                ldy     addr_mode
+                ldx     addressing_modes+1,y  ; instruction size
+                ldy     #0
+.byteloop:      lda     (code_pointer),y
+                jsr     JMP_PRINT_HEX
+                lda     #" "
+                jsr     JMP_PUTC
+                iny
+                dex
+                bne     .byteloop
+.spaceloop:     cpy     #4
+                beq     .next
+                lda     #" "
+                jsr     JMP_PUTC
+                lda     #" "
+                jsr     JMP_PUTC
+                lda     #" "
+                jsr     JMP_PUTC
+                iny
+                bra     .spaceloop
+.next:          jsr     print_instruction
+                inc16   code_pointer
+                lda     #CR
+                jsr     JMP_PUTC
+                lda     #LF
+                jsr     JMP_PUTC
+                plx
+                rts
+
+
+
 ;-------------------------------------------------------------
 ; This finds the instruction in the opcodes table. The opcodes
 ; table is 512 bytes long (256 entries of 2 bytes). The opcode
